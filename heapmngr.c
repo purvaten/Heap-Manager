f011f490766768dcdd8fa64ac4bf9b144b3c1f7b
Purva Tendulkar <purva.tendulkar@gmail.com>
2015-10-26 09:53:25 +0530

    heapmngr.c changed. my_free function added

--- heapmngr.c
+++ heapmngr.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "heapmngr.h"
 #include "chunk.h"
-#define MAX_SIZE	1000000	/* Units */
-#define NUM_BINS	1024
-
+#define MAX_SIZE			15	/* Units */
+#define NUM_BINS			1024
+#define MIN_UNITS_FROM_OS	1024
 
 /* INITIALLY ....................................................................*/
 
@@ -17,6 +18,26 @@ static Chunk_T freebinArray[NUM_BINS];
 /* Array of bins to doubly linked NUL terminated free lists of different bin sizes */
 
 /* ................................................................................ */
+
+/* TESTING PURPOSES............................................................ */
+
+void printbin()
+/* Prints entire link structure of all bins which are non empty */
+{
+	int i;
+	Chunk_T ptr;
+
+	for (i = 0; i < NUM_BINS; i++) {
+		ptr = freebinArray[i];
+		while (ptr != NULL) {
+			printf("%d : size : %d ptr : %p ptr->next : %p\n", i, (int)Chunk_getUnits(ptr), ptr, Chunk_getNextInList(ptr));
+			ptr = Chunk_getNextInList(ptr);
+		}
+	}
+} 
+
+/* ............................................................................. */
+
 
 int findBin(size_t Units)
 /* Returns correct bin size */
@@ -37,18 +58,21 @@ void removefromList(Chunk_T chunk_ptr)
 	if (previnList != NULL)
 		Chunk_setNextInList(previnList, nextinList);
 	else
-		freebinArray[chunk_ptr_val] = nextinList;
+		freebinArray[findBin(chunk_ptr_val)] = nextinList;
 	if (nextinList != NULL)
 		Chunk_setPrevInList(nextinList, previnList);
 }
 
-void InsertinBin(Chunk_T chunk_ptr, size_t chunk_size)
+void InsertinBin(Chunk_T chunk_ptr)
 /* Inserts chunk at beginning of link structure in respective bin */
 {	
 	Chunk_T binptr;
+	size_t chunk_size = Chunk_getUnits(chunk_ptr);
 
-	if (chunk_size > NUM_BINS - 1)
+	if (chunk_size > NUM_BINS - 1) {
 		binptr = freebinArray[NUM_BINS - 1];
+		chunk_size = NUM_BINS - 1;
+	}
 	else
 		binptr = freebinArray[chunk_size];
 
@@ -59,7 +83,7 @@ void InsertinBin(Chunk_T chunk_ptr, size_t chunk_size)
 
 Chunk_T useChunk(Chunk_T chunk_ptr, size_t Units)
 /* Uses the chunk. If chunk size is close to Units then remove the chunk from free list and return it.
-	If chunk size is too big split it and make re arrange link structure
+	If chunk size is too big split it and re arrange link structure
 	and return pointer to correct memory location slice */
 {	
 	size_t chunk_ptr_val = Chunk_getUnits(chunk_ptr);
@@ -89,15 +113,34 @@ Chunk_T useChunk(Chunk_T chunk_ptr, size_t Units)
 	Chunk_setStatus(splitchunk, CHUNK_FREE);
 
 	/* Depending on size of the split chunk, insert it in correct bin */
-	InsertinBin(splitchunk, splitchunk_size);
+	InsertinBin(splitchunk);
 
 	return temp_ptr;
 
 }
 
-Chunk_T getmoreMemory(Chunk_T chunk_ptr, size_t Units) {
+Chunk_T getmoreMemory(size_t Units)
+/* Mallocs additional memory enough to store Units number of units.
+	Create a new chunk, coalesce with prev and next chunks if free and place it at starting of array
+	in respective bin. Return pointer to this new chunk */
+{
 	return NULL;
 }
+
+Chunk_T Chunk_coalesce(Chunk_T a_chunk_ptr, Chunk_T b_chunk_ptr)
+/* Coalesces chunks pointed by a_chunk_ptr and b_chunk_ptr and returns pointer
+	to the newly created chunk */
+{	
+	size_t a_chunk_size = Chunk_getUnits(a_chunk_ptr);
+	size_t b_chunk_size = Chunk_getUnits(b_chunk_ptr);
+	size_t coalesce_chunk_size = a_chunk_size + b_chunk_size;
+
+	Chunk_setUnits(a_chunk_ptr, coalesce_chunk_size);
+	Chunk_setUnits(b_chunk_ptr, coalesce_chunk_size);
+
+	return a_chunk_ptr;
+}
+
 
 /* .................................................................................. */
 
@@ -116,14 +159,18 @@ void *my_malloc(size_t size)
 	/* Initialize if this is the first call */
 	if (HeapStart == NULL) {
 		HeapStart = (Chunk_T) malloc(MAX_SIZE * UnitSize);
+		Chunk_setUnits(HeapStart, MAX_SIZE);
 		/* Place this chunk in last bin */
-		InsertinBin(HeapStart, MAX_SIZE);
-		HeapEnd = HeapStart;
+		InsertinBin(HeapStart);
+		HeapEnd = (Chunk_T)((char *)HeapStart + MAX_SIZE * UnitSize);
 	}
 
 	/* Determine the number of units the new chunk should contain */
 	Units = ((size - 1) / UnitSize) + 1;
 	Units = Units + 2;	/* For Header and Footer */
+
+	printf("IN MALLOC : size : %d Units : %d\n", (int)size, (int)Units);
+	printbin();
 
 	/* Traverse through the array and look if bin of required size is available.
 		If it is, allocate it. If not, Check larger bins. */
@@ -135,6 +182,9 @@ void *my_malloc(size_t size)
 			if (Chunk_getUnits(chunk_ptr) >= Units) {
 				chunk_ptr = useChunk(chunk_ptr, Units);
 
+				printf("END OF MALLOC\n");
+				printbin();
+
 				return (void *)((char *)chunk_ptr + UnitSize);
 			}
 			chunk_ptr = Chunk_getNextInList(chunk_ptr);
@@ -142,20 +192,57 @@ void *my_malloc(size_t size)
 	}
 
 	/* Required memory is not found. Obtain new memory by doing malloc() */
-	chunk_ptr = getmoreMemory(chunk_ptr, Units);
+	chunk_ptr = getmoreMemory(Units);
 	if (chunk_ptr == NULL)
 	/* malloc failed */
 		return NULL;
 
 	/* Chunk is available for use */
 	chunk_ptr = useChunk(chunk_ptr, Units);
+
+	printf("END OF MALLOC : \n");
+	printbin();
 	return (void *)((char *)chunk_ptr + UnitSize);
 }
 
 /* ................................................................................ */
 
-int my_free(void *region)
-/* Free a previously allocated region. */
-{
-	return 0;
+void my_free(void *region)
+/* Free a previously allocated region. Region points to a region allocated by my_malloc().
+	If region is NULL do nothing */
+{	
+	printf("IN FREE ");
+	printbin();
+
+	size_t UnitSize = Chunk_getUnitSize();
+	Chunk_T chunk_ptr, PrevMem, NextMem;
+
+	if (region == NULL)
+		return;
+
+	chunk_ptr = (Chunk_T)((char*)region - UnitSize);
+
+	Chunk_setStatus(chunk_ptr, CHUNK_FREE);
+	NextMem = Chunk_getNextInMem(chunk_ptr, HeapEnd);
+	PrevMem = Chunk_getPrevInMem(chunk_ptr, HeapStart);
+
+	/* If PrevMem is also free coalesce the two chunks */
+	if (PrevMem != NULL && Chunk_getStatus(PrevMem) == CHUNK_FREE) {
+		removefromList(PrevMem);
+
+		chunk_ptr = Chunk_coalesce(chunk_ptr, PrevMem);
+	}
+	
+	/* If NextMem is also free coalesce the two chunks */
+	if (NextMem != NULL && Chunk_getStatus(NextMem) == CHUNK_FREE) {
+		removefromList(NextMem);
+
+		chunk_ptr = Chunk_coalesce(chunk_ptr, NextMem);
+	}
+
+	/* Place final bigger chunk in strating of linked structure for correct bin */
+	InsertinBin(chunk_ptr);
+
+	printf("END OF FREE\n");
+	printbin();
 }
